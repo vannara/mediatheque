2041edbaf622d61d1e66daa8bd9f477f4847a249
vannara <vannara.loch@gmail.com>
2013-11-16 20:46:31 +0100

    auto generated id for card

--- Mediatheque/src/java/enterprise/web_jpa_war/entity/Card.java
+++ Mediatheque/src/java/enterprise/web_jpa_war/entity/Card.java
@@ -8,6 +8,8 @@ package enterprise.web_jpa_war.entity;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
@@ -20,6 +22,7 @@ public class Card implements Serializable {
     private static final long serialVersionUID = 1L;
     
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String login;
     private boolean isLost;
     private double guaranteeAmt;
